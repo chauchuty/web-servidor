8d912b9262f1227ea85cf7e315fb5949d85f3bec
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-04-08 00:02:41 -0300

    update: login page style

--- html/db.php
+++ html/db.php
@@ -1,17 +1,17 @@
 <?php 
-    // $db_host = 'webservidor.c9kxg983k5ek.us-east-1.rds.amazonaws.com';
-    // $db_name = 'webservidor';
-    // $db_user = 'root';
-    // $db_pass = '!!010203';
-    $db_host = '127.0.0.1';
+    $db_host = 'webservidor.c9kxg983k5ek.us-east-1.rds.amazonaws.com';
     $db_name = 'webservidor';
     $db_user = 'root';
     $db_pass = '!!010203';
+    
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
     
     try {
         $db = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_user, $db_pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
-    }    
+    }
+
+    
 ?>
--- html/views/login.view.php
+++ html/views/login.view.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php include('../components/header.php') ?>
+<?php include('../components/header.php') ?>
 
     <main class="login-img">
-        
     </main>
     <main class="login">
     <img src="../assets/images/img-logo.webp" alt="" class="login-img">
