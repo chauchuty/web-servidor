295e71ebf8d793497d7557b208a8024fca9d168d
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-04-08 23:54:43 -0300

    update: sidebar

--- html/components/sidebar.php
+++ html/components/sidebar.php
@@ -3,37 +3,40 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
-        <i class="fas fa-trophy"></i>
+            <i class="fas fa-trophy"></i>
         </div>
         <div class="sidebar-brand-text mx-3">WORLD CUP <sup>2022</sup></div>
     </a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
+
+    <div class="sidebar-heading">
+        Menu Principal
+    </div>
+
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
         <a class="nav-link" href="../pages/dashboard.php">
-        <i class="fas fa-home"></i>
-            <span>HOME</span></a>
+            <i class="fas fa-home"></i>
+            <span>Dashboard</span></a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link" href="../pages/calendario.php">
-        <i class="fas fa-calendar-alt"></i>
-            <span>CALENDÁRIO</span></a>
+            <i class="fas fa-calendar-alt"></i>
+            <span>Calendário</span></a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link" href="index.html">
-        <i class="fas fa-coins"></i>
-            <span>APOSTAS</span></a>
+            <i class="fas fa-coins"></i>
+            <span>Apostas</span></a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link" href="index.html">
-        <i class="fas fa-chart-line"></i>
-            <span>RESULTADOS</span></a>
+            <i class="fas fa-chart-line"></i>
+            <span>Resultados</span></a>
     </li>
 
     <!-- Divider -->
@@ -44,37 +47,12 @@
         USUÁRIO
     </div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-        <i class="fas fa-user"></i>
-            <span>PERFIL</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-chart-line"></i>
+            <span>Minhas Apostas</span></a>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
-    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider">
@@ -83,8 +61,6 @@
     <div class="sidebar-heading">
         ADMIN
     </div>
-
-    
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
@@ -103,10 +79,15 @@
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-    
-    <button class="btn btn-light"><i class="fas fa-door-open"></i>SAIR</button>
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Opções
     </div>
+
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-door-open"></i>
+            <span>Sair</span></a>
+    </li>
 
 </ul>
